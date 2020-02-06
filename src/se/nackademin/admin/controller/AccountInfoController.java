1cf7555af595fdb8f9b154dad7e270c4cac1c020
”Jacob <jacob.swenson@outlook.com>
2020-02-06 01:35:33 +0100

    added controllers and buttons

--- src/se/nackademin/admin/controller/AccountInfoController.java
+++ src/se/nackademin/admin/controller/AccountInfoController.java
@@ -62,67 +62,6 @@ public class AccountInfoController {
 
     }
 
-    class depositInAccount implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            int accountId = controller.getPanelHandler().getAccountInfo().getCurrentAccount().getId();
-            String amount = controller.getPanelHandler().getAccountInfo().getWithdrawSum().getText();
-            int minusAmount = Integer.parseInt(amount);
-
-
-            minusAmount *= -1;
-            controller.getRepository().updateBalanceForAccount(accountId,minusAmount);
-            List<Account> accounts = controller.getRepository().getAccountFromCustomer(controller.getCurrentCustomer());
-            accounts.forEach(a -> {
-//                if(a.getId() == controller.getPanelHandler().getAccountInfo().getCurrentAccount().getId()) {
-//                    controller.getPanelHandler().changeToAccountPanel()                           //Lägg till den panel som ska va här
-//                }
-            });
-//            int accountId = controller.getPanelHandler().getAccountInfo().getCurrentAccount().getId();
-//            String amount = controller.getPanelHandler().getAccountInfo().getWithdrawSum().getText();
-//            int minusAmount = Integer.parseInt(amount);
-//
-//
-//            minusAmount *= -1;
-//            controller.getRepository().updateBalanceForAccount(accountId,minusAmount);
-//            List<Account> accounts = controller.getRepository().getAccountFromCustomer(controller.getCurrentCustomer());
-//            accounts.forEach(a -> {
-//                if(a.getId() == controller.getPanelHandler().getAccountInfo().getCurrentAccount().getId()) {
-//                    controller.getPanelHandler().changeToAccountPanel()                           //Lägg till den panel som ska va här
-//                }
-//            });
-
-        }
-    }
-    class FilterDate implements ActionListener {
-
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String startDate = controller.getPanelHandler().getAccountInfo().getStartDate().getText();
-            String endDate = controller.getPanelHandler().getAccountInfo().getEndDate().getText();
-
-            if (DateValidator.isValid(startDate) && DateValidator.isValid(endDate)) {
-                if (DateValidator.isStartDateBeforeEndDate(startDate,endDate)) {
-
-                    List<AccountHistory> filterdAccounts = controller.getRepository().getAccountHistory(startDate,endDate);
-                    filterdAccounts.forEach(a -> {
-//                if(a.getId() == controller.getPanelHandler().getAccountInfo().getCurrentAccount().getId()) {
-//                    controller.getPanelHandler().changeToAccountPanel()                           //Lägg till den panel som ska va här
-//                }
-                    });
-
-                } else {
-                    JOptionPane.showMessageDialog(null, "datumen är i fel ordning.");
-                }
-            } else {
-                JOptionPane.showMessageDialog(null, "ej korrekta datum, ange YYYY-MM-DD");
-            }
-
-        }
-    }
-
     public ActionListener getAccountInfoListener() {
         return new AccountinfoListener();
     }
